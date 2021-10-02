7d6fd81ef630abaebb6501c500bcba8b59983b15
bigveloper <bigveloper@gmail.com>
2021-10-02 10:22:45 +0900

    JSON

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
-import printME from './printMe';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-    return <printME />;
+    // state
+    const [data, setData] = useState(null);
+
+    // event
+    const onClick = () => {
+        axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
+            setData(response.data);
+        });
+    };
+
+    return (
+        <div>
+            <div>
+                <button onClick={onClick}>불러오기</button>
+            </div>
+            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+        </div>
+    );
 }
 
 export default App;
