c75200056951693ba9e870c2691919689a25cc05
bigveloper <bigveloper@gmail.com>
2021-10-02 12:43:55 +0900

    categories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
 function App() {
-    return <NewsList />;
+    return (
+        <>
+            <Categories />
+            <NewsList />
+        </>
+    );
 }
 export default App;
