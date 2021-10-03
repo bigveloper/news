7559cf3eaac842d879f55ec5269c93d526849527
bigveloper <bigveloper@gmail.com>
2021-10-03 21:32:02 +0900

    news Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { useState } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-    const [category, setCategory] = useState('all');
-    const onSelect = (category) => (setCategory(category), []);
-    return (
-        <>
-            <Categories category={category} onSelect={onSelect} />
-            <NewsList category={category} />
-        </>
-    );
+    return <Route path='/:category?' component={NewsPage} />;
 };
 
 export default App;
