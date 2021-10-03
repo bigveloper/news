41781e887116bf009768a5c9e980766ff5a84ebc
bigveloper <bigveloper@gmail.com>
2021-10-03 20:31:48 +0900

    NewsItem,List

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from '../../node_modules/axios/index';
 import NewsItem from './NewItem';
 
 const NewsListBlock = styled.div`
@@ -14,22 +16,36 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160',
-};
-
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=7ae88fdae1b94a049bf209bdbf27cd88');
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <NewsListBlock>대기중...</NewsListBlock>;
+    }
+    if (!articles) {
+        return null;
+    }
+
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map((article) => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     );
 };
