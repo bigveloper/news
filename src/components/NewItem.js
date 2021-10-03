7579098b83fdcf58a86c48e262e56b98898ddb57
bigveloper <bigveloper@gmail.com>
2021-10-03 19:47:19 +0900

    NewsList Ui

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -31,4 +31,27 @@ const NewsItemBlock = styled.div`
     }
 `;
 
+function NewsItem({ article }) {
+    const { title, description, url, urlToImage } = article;
+    return (
+        <NewsItemBlock>
+            {urlToImage && (
+                <div className='thumbnail'>
+                    <a href={url} target='_blank' rel='noopener noreferrer'>
+                        <img src={urlToImage} alt='thumbnail' />
+                    </a>
+                </div>
+            )}
+            <div className='contents'>
+                <h2>
+                    <a href={url} target='_blank' rel='noopener noreferrer'>
+                        {title}
+                    </a>
+                </h2>
+                <p>{description}</p>
+            </div>
+        </NewsItemBlock>
+    );
+}
+
 export default NewsItem;
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import NewsItem from './NewItem';
 
-const NewsItemBlock = styled.div`
+const NewsListBlock = styled.div`
     box-sizing: border-box;
     padding-bottom: 3rem;
     width: 768px;
@@ -12,3 +13,25 @@ const NewsItemBlock = styled.div`
         padding-right: 1rem;
     }
 `;
+
+const sampleArticle = {
+    title: '제목',
+    description: '내용',
+    url: 'https://google.com',
+    urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+    return (
+        <NewsListBlock>
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+        </NewsListBlock>
+    );
+};
+
+export default NewsList;
